fdf249865d67d5966f8ff2664098b187f1c0fab7
lunxkoe <lunxkoe@gmail.com>
2026-07-02 00:03:03 +0900

    chore: add SwaggerConfig.java

---
+++ src/main/java/lunxkoe/practiceinitialsettingsv1/global/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package lunxkoe.practiceinitialsettingsv1.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile({"dev"})
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API 문서")
+                        .description("API 문서입니다.")
+                        .version("0.0.1")
+                )
+                .servers(List.of(
+                                new Server().url("http://localhost:8080").description("Local Server")
+                        )
+                );
+    }
+}
