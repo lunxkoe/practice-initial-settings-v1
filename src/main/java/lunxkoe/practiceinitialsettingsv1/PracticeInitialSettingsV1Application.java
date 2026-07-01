1236b23dccd4ead653f914c22d3a7f876923cf13
lunxkoe <lunxkoe@gmail.com>
2026-07-01 23:33:30 +0900

    chore: initial project

---
+++ src/main/java/lunxkoe/practiceinitialsettingsv1/PracticeInitialSettingsV1Application.java
@@ -0,0 +1,13 @@
+package lunxkoe.practiceinitialsettingsv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeInitialSettingsV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeInitialSettingsV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/lunxkoe/practiceinitialsettingsv1/PracticeInitialSettingsV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package lunxkoe.practiceinitialsettingsv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeInitialSettingsV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
